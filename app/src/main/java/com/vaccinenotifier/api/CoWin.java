3f109f8de12cd58782837eaf6bb61b6cff246f18
Rajul Babel <babelrajul@gmail.com>
2021-06-13 20:12:42 +0530

    update cowin api uri

--- app/src/main/java/com/vaccinenotifier/api/CoWin.java
+++ app/src/main/java/com/vaccinenotifier/api/CoWin.java
@@ -9,6 +9,6 @@ import retrofit2.http.Query;
 
 public interface CoWin {
 
-    @GET("/api/v2/appointment/sessions/calendarByDistrict")
+    @GET("/api/v2/appointment/sessions/public/calendarByDistrict")
     Call<GetSlotsResponse> getSlots(@Query("district_id") Integer districtId, @Query("date") String date, @Header("User-Agent") String userAgent);
 }
